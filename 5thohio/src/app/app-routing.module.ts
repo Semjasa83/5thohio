5386a1ba508dd694488e0147b6eea63a3131679a
MarceldeZanna <94352026+Semjasa83@users.noreply.github.com>
2023-10-01 17:51:08 +0200

    Merge pull request #1
    * home and routes

--- 5thohio/src/app/app-routing.module.ts
+++ 5thohio/src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',   redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: 'home', component: HomeComponent,
+    // children: [
+    //   { path: '', redirectTo: 'summary', pathMatch: 'prefix' },
+    //   { path: 'summary', component: SummaryComponent },
+    //   { path: 'board', component:  BoardComponent },
+    //   { path: 'contacts', component: ContactsComponent,
+    //     children: [
+    //       { path: '',   redirectTo: 'contacts', pathMatch: 'full' },
+    //       { path: 'contactdetail/:id', component: ContactDetailComponent }
+    //     ]},
+    //   { path: 'addtask', component: TaskAddRouterComponent}
+    // ]
+  },
+  { path: '**', redirectTo: 'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
